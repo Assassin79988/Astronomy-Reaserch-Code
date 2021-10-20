63730a1ad87bd15e57515450a88a9babc427850b
= <=>
2021-10-20 13:49:00 -0300

    commit

--- calcPhysicalDist.py
+++ calcPhysicalDist.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random, time, os
 from astropy.stats import bootstrap
+from scipy.stats import spearmanr, pearsonr, kendalltau
+import pickle
 
 """
 Name: bootstrap(x,N,statfunc = None)
@@ -165,8 +167,7 @@ def findJPEG(directory = "./", option = 's'):
 
     return filePaths
 
-if __name__ == '__main__':
-
+def getPhysicalDistDict():
     fileDir = "../HeCS_omnibus_dn4000.fits"
     data = fits.fetchFITS(fileDir, 's')
 
@@ -187,7 +188,12 @@ if __name__ == '__main__':
         physDict[centerData[i,0]] = np.array(temp)
         print(clusterDict[centerData[i,0]][0][0], "Done.")
 
+        return physDict
 
+
+if __name__ == '__main__':
+    
+    physDict = getPhysicalDict()
 
     _Trend = findJPEG("./Rcl_Visual_Trend")
     Trend = []
@@ -209,10 +215,18 @@ if __name__ == '__main__':
     
     #Dn4000 Med Plots
 
+    accept = 0
+    reject = 0
+
+    S, Sp = [], []
+    P, Pp = [], []
+    K, Kp = [], []
+    #fig = plt.figure(figsize=(20,10))
     for cluster in fits.clusters:
         Dn4000 = []
         Dnerr = []
         Rcl = []
+
         for data in physDict[cluster]:
             if(np.float64(data[5]) > 0 and np.float64(np.float64(data[6])) < 1):
                 Dn4000.append(np.float64(data[5]))
@@ -223,7 +237,8 @@ if __name__ == '__main__':
         Dn4000 = np.array(Dn4000, dtype= np.float64)
         Dnerr = np.array(Dnerr, dtype = np.float64)
 
-        binSize = 200 * 0.001# Mpc
+        #binSize = 200 * 0.001# Mpc
+        binSize = 400 * 0.001# Mpc
         RclMax = np.max(Rcl)
         binNum = int(math.ceil(RclMax/ binSize))
         
@@ -243,22 +258,130 @@ if __name__ == '__main__':
                 RclMean.append(np.mean(Rcl[indexBins[i]]))
         #print(Rcl[indexBins[-1]])
         #print(RclMean)
+        #X.append(np.array(RclMean))
+        #Y.append(np.array(Dn4000Med))
+        #Yerr.append(np.array(DnerrMed))
+        
+        coef, p = spearmanr(Rcl, Dn4000)
+        One = np.where(Rcl < 1)
+        Two = np.where(Rcl < 2)
+        Four = np.where(Rcl < 4)
+        
+        o,po = pearsonr(Rcl[One], Dn4000[One])
+        t, pt = pearsonr(Rcl[Two], Dn4000[Two])
+        a, pa = pearsonr(Rcl, Dn4000)
+
+        _o,_po = spearmanr(Rcl[One], Dn4000[One])
+        _t, _pt = spearmanr(Rcl[Two], Dn4000[Two])
+        _a, _pa = spearmanr(Rcl, Dn4000)
+
+        __o,__po = kendalltau(Rcl[One], Dn4000[One])
+        __t, __pt = kendalltau(Rcl[Two], Dn4000[Two])
+        __a, __pa = kendalltau(Rcl, Dn4000)
+
+        ps = '< 1 MeV Person-r:%.2f\tp-value:%.2f\n < 2 MeV Person-r:%.2f\tp-value:%.2f\nPerson-r:%.2f\tp-value:%.2f'%(o, po, t, pt, a, pa)
+        ss = '< 1 MeV Spearman-r:%.2f\tp-value:%.2f\n < 2 MeV Spearman-r:%.2f\tp-value:%.2f\nSpearman-r:%.2f\tp-value:%.2f'%(_o, _po, _t, _pt, _a, _pa)
+        ks = '< 1 MeV Kendalltau:%.2f\tp-value:%.2f\n < 2 MeV Kendalltau:%.2f\tp-value:%.2f\nKendalltau:%.2f\tp-value:%.2f'%(__o, __po, __t, __pt, __a, __pa)
+
+        S.append(spearmanr(Rcl, Dn4000)[0])
+        Sp.append(spearmanr(Rcl, Dn4000)[1])
+        P.append(pearsonr(Rcl, Dn4000)[0])
+        Pp.append(pearsonr(Rcl, Dn4000)[1])
+        K.append(kendalltau(Rcl, Dn4000)[0])
+        Kp.append(kendalltau(Rcl, Dn4000)[1])
+
+        #print("\n\n\n\n\n\n\n\n\n")
+        #print("--------------------- < 1MeV ----------------------")
+        #print("Spearmanr: ",spearmanr(Rcl[One], Dn4000[One]))
+        #print("Pearsonr: ", pearsonr(Rcl[One], Dn4000[One]))
+        #print("kendalltau: ", kendalltau(Rcl[One], Dn4000[One]))
+
+        #print("---------------------- < 2 MeV --------------------")
+        #print("Spearmanr: ",spearmanr(Rcl[Two], Dn4000[Two]))
+        #print("Pearsonr: ", pearsonr(Rcl[Two], Dn4000[Two]))
+        #print("kendalltau: ", kendalltau(Rcl[Two], Dn4000[Two]))
+
+        #print("---------------------- < 4 MeV --------------------")
+        #print("Spearmanr: ",spearmanr(Rcl[Four], Dn4000[Four]))
+        #print("Pearsonr: ", pearsonr(Rcl[Four], Dn4000[Four]))
+        #print("kendalltau: ", kendalltau(Rcl[Four], Dn4000[Four]))
+        
+        fig = plt.figure(figsize=(20,10))
+        plt.scatter(Rcl, Dn4000, s = 2, alpha = 0.5, c = 'r')
         plt.scatter(RclMean, Dn4000Med, s = 2)
         try:
             plt.errorbar(RclMean, Dn4000Med, yerr = DnerrMed, linestyle = 'None')
         except:
-            print(DnerrMed)
+            print("\n\n\nERROR HAD OCCURED\n\n\n")
             break
         #plt.scatter(Rcl, Dn4000, s = 2, c = 'r')
         #plt.show()
         plt.xlabel("Rcl (Mpc)")
         plt.ylabel("Dn4000")
         plt.title("Dn4000 Vs Rcl for {clust}".format(clust = cluster))
-        plt.xlim(0,2)
+        plt.figtext(0.02, 0.5, ps + '\n\n' + ss + '\n\n' + ks, fontsize = 14)
+        plt.subplots_adjust(left=0.25)
+        if (po > 0.05):
+            accept += 1
+            #print("Null accepted")
+            #print("P-Value: ", po)
+            #plt.show()
+        else:
+            reject += 1
+            #print("Null rejected")
+            #plt.cla()
+
         plt.savefig("Rcl/" + cluster + ".jpeg")
         plt.cla()
-    #plt.show()
+        plt.close()
+        #plt.show()
+        
+    print("Null Accepted: ",accept)
+    print("Null Rejected: ",reject)
+    fig, ax = plt.subplots(1,2)
+    fig.suptitle("Spearman-r (All Data)")
+    ax[0].hist(S)
+    ax[1].hist(Sp)
+    ax[0].set(xlabel = "Correlation coefficient")
+    ax[1].set(xlabel="P-Value")
+    plt.show()
+    fig, ax = plt.subplots(1,2)
+    fig.suptitle("Kendall-tau (All Data)")
+    ax[0].hist(K)
+    ax[1].hist(Kp)
+    ax[0].set(xlabel = "Correlation coefficient")
+    ax[1].set(xlabel="P-Value")
+    plt.show()
+    fig, ax = plt.subplots(1,2)
+    fig.suptitle("Pearson-r (All Data)")
+    ax[0].hist(P)
+    ax[1].hist(Pp)
+    ax[0].set(xlabel = "Correlation coefficient")
+    ax[1].set(xlabel="P-Value")
+    plt.show()
     
+    """
+    X = np.array(X, dtype='object')
+    Y = np.array(Y, dtype='object')
+    Yerr = np.array(Yerr, dtype='object')
+
+    X = X.T
+    Y = Y.T
+    Yerr = Yerr.T
+
+    RclMean = []
+    Dn4000Med = []
+    DnerrMed = []
+    for i in range(len(X)):
+        print(X.T[i])
+        RclMean.append(np.mean(X[i]))
+        Dn4000Med.append(np.median(Y[i]))
+        DnerrMed.append(np.std(bootstrap(Y[i], 500, bootfunc = np.median))) 
+
+    plt.scatter(RclMean, Dn4000Med, s = 2)
+    plt.errorbar(RclMean, Dn4000Med, yerr=DnerrMed, linestyle='None')
+    plt.show()
+    """
     """
     plt.errorbar(Rcl, Dn4000, yerr = Dnerr, linestyle = 'None')
     plt.xlabel("Rcl (Mpc)")
